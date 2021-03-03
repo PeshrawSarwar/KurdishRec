a2dacb94196d03160e1f5873b2c29334e2f1a19a
Peshraw Sarwar <peshrawsarwar@gmail.com>
2021-03-03 15:09:53 +0300

    last changes

--- resources/views/post/post.blade.php
+++ resources/views/post/post.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Rest & Cafe Review</title>
 
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
